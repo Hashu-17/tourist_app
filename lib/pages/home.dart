8dba67abdbbd87741ae90773a3b6d9760ae1c7f0
Hashua <neutrinohash2@gmail.com>
2025-08-26 14:25:36 +0530

    user icon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,24 +10,55 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: 
-            Container(child: 
-             Stack(children:[
-             Image.asset("images/home.png", width: MediaQuery.of(context).size.width, height:MediaQuery.of(context).size.height/2.5, fit: BoxFit.cover,),
-             Padding(
-               padding: const EdgeInsets.only(top:50.0, right:10.0,),
-               child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                 children: [
-                   ClipRRect(
-                    borderRadius: BorderRadius.circular(25),
-                    child: Image.asset("images/user.png", width:50, height: 50, fit: BoxFit.cover,)),
-                 ],
-               ),
-             )
-        ],
-      ),
-     )
-    );
+    return Scaffold(
+        body: Container(
+      child: Stack(children: [
+        Image.asset(
+          "images/home.png",
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 2.5,
+          fit: BoxFit.cover,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 50.0,
+            right: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Material(
+                elevation: 3.0,
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(20), // less rounded for a boxy look
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), // boxy rounded corners
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 186, 247, 244), // light greenish
+                        Color.fromARGB(255, 241, 246, 247), // light blue
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(4),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16), // match or slightly less than container
+                    child: Image.asset(
+                      "images/user.png",
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
+    ));
   }
 }
