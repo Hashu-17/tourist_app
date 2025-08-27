9537c2b0e4b1c6ca0e5aa06c44572f0511178025
Hashua <neutrinohash2@gmail.com>
2025-08-27 16:49:03 +0530

    home page updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(20), // boxy rounded corners
                     gradient: LinearGradient(
                       colors: [
-                        Color.fromARGB(255, 186, 247, 244), // light greenish
+                        Colors.teal.shade100, // light greenish
                         Color.fromARGB(255, 241, 246, 247), // light blue
                       ],
                       begin: Alignment.topLeft,
@@ -48,8 +48,8 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(16), // match or slightly less than container
                     child: Image.asset(
                       "images/user.png",
-                      width: 50,
-                      height: 50,
+                      width: 40,
+                      height: 40,
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -57,8 +57,54 @@ class _HomeState extends State<Home> {
               ),
             ],
           ),
-        )
-      ]),
-    ));
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 149.55, left: 20.0),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            
+          Text("MegTour",
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,fontFamily: 'Lato'
+          ),
+          ),
+          Text("A tourist's guide",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,fontFamily: 'Lato',
+            height: 0,
+           ),
+          ),
+          ],
+         ),
+        ),
+       Container(
+         margin: EdgeInsets.only(left: 30, right: 30, top: MediaQuery.of(context).size.height / 2.8),
+         child: Material(
+          elevation:5.0,
+          borderRadius: BorderRadius.circular(30),
+           child: Container(
+                 
+                 decoration: BoxDecoration(color: Colors.white, border: Border.all(width:1.5),borderRadius: BorderRadius.circular(30)),
+  
+                 child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.search),
+              hintText: 'Search your destination',
+              hintStyle: TextStyle(color: Colors.grey,fontFamily: 'Lato'),
+            ),
+           ),
+                ),
+         ),
+       )
+    ],
+    ),
+  ),
+);
   }
 }
